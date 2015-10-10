cfebbc5355b17405571c15be737c117019664b6b
NoSyu <dongdm@gmail.com>
2015-10-10 20:25:30 +0900

    Almost complete it

--- Models/LDA/VariationalEM.py
+++ Models/LDA/VariationalEM.py
@@ -6,62 +6,11 @@ import numpy
 import scipy.special
 
 
-def parse_doc_list(docs, vocab):
-    """
-    Parse a document into a list of word ids and a list of counts,
-    or parse a set of documents into two lists of lists of word ids
-    and counts.
-
-    Arguments: 
-    docs:  List of D documents. Each document must be represented as
-           a single string. (Word order is unimportant.) Any
-           words not in the vocabulary will be ignored.
-    vocab: Dictionary mapping from words to integer ids.
-
-    Returns a pair of lists of lists. 
-
-    The first, wordids, says what vocabulary tokens are present in
-    each document. wordids[i][j] gives the jth unique token present in
-    document i. (Don't count on these tokens being in any particular
-    order.)
-
-    The second, wordcts, says how many times each vocabulary token is
-    present. wordcts[i][j] is the number of times that the token given
-    by wordids[i][j] appears in document i.
-    """
-    if (type(docs).__name__ == 'str'):
-        temp = list()
-        temp.append(docs)
-        docs = temp
-
-    D = len(docs)
-    
-    wordids = list()
-    wordcts = list()
-    for d in range(0, D):
-        docs[d] = docs[d].lower()
-        docs[d] = re.sub(r'-', ' ', docs[d])
-        docs[d] = re.sub(r'[^a-z ]', '', docs[d])
-        docs[d] = re.sub(r' +', ' ', docs[d])
-        words = string.split(docs[d])
-        ddict = dict()
-        for word in words:
-            if (word in vocab):
-                wordtoken = vocab[word]
-                if (not wordtoken in ddict):
-                    ddict[wordtoken] = 0
-                ddict[wordtoken] += 1
-        wordids.append(ddict.keys())
-        wordcts.append(ddict.values())
-
-    return((wordids, wordcts))
-
-
 class VariationalEM:
     """
     Implements Variational EM for LDA
     """
-    def __init__(self, vocab, K, D, alpha, eta, tau0, kappa, small_num=1e-100, meanchangethresh=0.001):
+    def __init__(self, numtopic, docs, words, alpha=0.1, eta=0.01, small_num=1e-100):
         """
         Arguments:
         K: Number of topics
@@ -77,23 +26,15 @@ class VariationalEM:
         kappa: Learning rate: exponential decay rate---should be between
              (0.5, 1.0] to guarantee asymptotic convergence.
         """
-        self._vocab = dict()
-        for word in vocab:
-            word = word.lower()
-            word = re.sub(r'[^a-z]', '', word)
-            self._vocab[word] = len(self._vocab)
+        self._K = numtopic
+        self._words = words
+        self._docs = docs
 
-        self._K = K
-        self._W = len(self._vocab)
-        self._D = D
+        self._W = len(self._words)
+        self._D = len(self._docs)
         self._alpha = alpha
         self._eta = eta
-        self._tau0 = tau0 + 1
-        self._kappa = kappa
-        self._updatect = 0
-
         self._small_num = small_num
-        self._meanchangethresh = meanchangethresh
 
         # Initialize the variational distribution q(beta|lambda)
         self._lambda = numpy.random.gamma(100., 1./100., (self._K, self._W))
@@ -108,7 +49,7 @@ class VariationalEM:
             return scipy.special.psi(alpha) - scipy.special.psi(numpy.sum(alpha))
         return scipy.special.psi(alpha) - scipy.special.psi(numpy.sum(alpha, 1))[:, numpy.newaxis]
 
-    def _e_step(self, docs):
+    def _e_step(self, e_max_iter, meanchangethresh):
         """
         Given a mini-batch of documents, estimates the parameters
         gamma controlling the variational distribution over the topic
@@ -122,27 +63,22 @@ class VariationalEM:
         Returns a tuple containing the estimated values of gamma,
         as well as sufficient statistics needed to update lambda.
         """
-        wordids, wordcts = parse_doc_list(docs, self._vocab)
-        batchD = len(docs)
-
         # Initialize the variational distribution q(theta|gamma) for
         # the mini-batch
-        gamma = numpy.random.gamma(100., 1./100., (batchD, self._K))
+        gamma = numpy.random.gamma(100., 1./100., (self._D, self._K))
         Elogtheta = self._dirichlet_expectation(gamma)
         expElogtheta = numpy.exp(Elogtheta)
-
         sstats = numpy.zeros(self._lambda.shape)
 
         # Now, for each document d update that document's gamma and phi
-        it = 0
-        meanchange = 0
-        for d in xrange(0, batchD):
+        # for d in xrange(0, batchD):
+        for doc_idx, doc in enumerate(self._docs):
             # These are mostly just shorthand (but might help cache locality)
-            ids = wordids[d]
-            cts = wordcts[d]
-            gammad = gamma[d, :]
+            ids = doc.wordids
+            cts = doc.wordcts
+            gammad = gamma[doc_idx, :]
             # Elogthetad = Elogtheta[d, :]
-            expElogthetad = expElogtheta[d, :]
+            expElogthetad = expElogtheta[doc_idx, :]
             expElogbetad = self._expElogbeta[:, ids]
 
             # The optimal phi_{dwk} is proportional to 
@@ -150,7 +86,7 @@ class VariationalEM:
             phinorm = numpy.dot(expElogthetad, expElogbetad) + self._small_num
 
             # Iterate between gamma and phi until convergence
-            for it in xrange(0, 100):
+            for _ in xrange(e_max_iter):
                 lastgamma = gammad
                 # We represent phi implicitly to save memory and time.
                 # Substituting the value of the optimal phi back into
@@ -161,9 +97,9 @@ class VariationalEM:
                 phinorm = numpy.dot(expElogthetad, expElogbetad) + self._small_num
                 # If gamma hasn't changed much, we're done.
                 meanchange = numpy.mean(abs(gammad - lastgamma))
-                if meanchange < self._meanchangethresh:
+                if meanchange < meanchangethresh:
                     break
-            gamma[d, :] = gammad
+            gamma[doc_idx, :] = gammad
             # Contribution of document d to the expected sufficient
             # statistics for the M step.
             sstats[:, ids] += numpy.outer(expElogthetad.T, cts/phinorm)
@@ -176,7 +112,7 @@ class VariationalEM:
 
         return gamma, sstats
 
-    def run(self, docs):
+    def run(self, max_iter=10, e_max_iter=100, meanchangethresh=0.001, do_print_log=False):
         """
         First does an E step on the mini-batch given in wordids and
         wordcts, then uses the result of that E step to update the
@@ -200,73 +136,65 @@ class VariationalEM:
         # Do an E step to update gamma, phi | lambda for this
         # mini-batch. This also returns the information about phi that
         # we need to update lambda.
-        gamma, sstats = self.do_e_step(docs)
+        if do_print_log:
+            prev = time.time()
+            num_words_docs = 0
+            for doc in self._docs:
+                num_words_docs += sum(doc.wordcts)
 
-        # Estimate held-out likelihood for current values of lambda.
-        bound = self.approx_bound(docs, gamma)
+        for idx in xrange(max_iter):
+            gamma, sstats = self._e_step(e_max_iter, meanchangethresh)
 
-        # Update lambda based on documents
-        self._lambda = self._eta + self._D * sstats / len(docs)
-        self._Elogbeta = self._dirichlet_expectation(self._lambda)
-        self._expElogbeta = numpy.exp(self._Elogbeta)
-        self._updatect += 1
+            # Update lambda based on documents
+            self._lambda = self._eta + sstats
+            self._Elogbeta = self._dirichlet_expectation(self._lambda)
+            self._expElogbeta = numpy.exp(self._Elogbeta)
 
-        return gamma, bound
+            if do_print_log:
+                bound = self._approx_bound(gamma)
+                perwordbound = bound / num_words_docs
+                print '{}\t{}\theld-out perplexity estimate:\t{}'.format(idx, time.time() - prev, numpy.exp(-perwordbound))
+                prev = time.time()
 
-#     def approx_bound(self, docs, gamma):
-#         """
-#         Estimates the variational bound over *all documents* using only
-#         the documents passed in as "docs." gamma is the set of parameters
-#         to the variational distribution q(theta) corresponding to the
-#         set of documents passed in.
-#
-#         The output of this function is going to be noisy, but can be
-#         useful for assessing convergence.
-#         """
-#
-#         # This is to handle the case where someone just hands us a single
-#         # document, not in a list.
-#         if (type(docs).__name__ == 'string'):
-#             temp = list()
-#             temp.append(docs)
-#             docs = temp
-#
-#         (wordids, wordcts) = parse_doc_list(docs, self._vocab)
-#         batchD = len(docs)
-#
-#         score = 0
-#         Elogtheta = dirichlet_expectation(gamma)
-#         expElogtheta = n.exp(Elogtheta)
-#
-#         # E[log p(docs | theta, beta)]
-#         for d in range(0, batchD):
-#             gammad = gamma[d, :]
-#             ids = wordids[d]
-#             cts = n.array(wordcts[d])
-#             phinorm = n.zeros(len(ids))
-#             for i in range(0, len(ids)):
-#                 temp = Elogtheta[d, :] + self._Elogbeta[:, ids[i]]
-#                 tmax = max(temp)
-#                 phinorm[i] = n.log(sum(n.exp(temp - tmax))) + tmax
-#             score += n.sum(cts * phinorm)
-# #             oldphinorm = phinorm
-# #             phinorm = n.dot(expElogtheta[d, :], self._expElogbeta[:, ids])
-# #             print oldphinorm
-# #             print n.log(phinorm)
-# #             score += n.sum(cts * n.log(phinorm))
-#
-#         # E[log p(theta | alpha) - log q(theta | gamma)]
-#         score += n.sum((self._alpha - gamma)*Elogtheta)
-#         score += n.sum(gammaln(gamma) - gammaln(self._alpha))
-#         score += sum(gammaln(self._alpha*self._K) - gammaln(n.sum(gamma, 1)))
-#
-#         # Compensate for the subsampling of the population of documents
-#         score = score * self._D / len(docs)
-#
-#         # E[log p(beta | eta) - log q (beta | lambda)]
-#         score = score + n.sum((self._eta-self._lambda)*self._Elogbeta)
-#         score = score + n.sum(gammaln(self._lambda) - gammaln(self._eta))
-#         score = score + n.sum(gammaln(self._eta*self._W) -
-#                               gammaln(n.sum(self._lambda, 1)))
-#
-#         return(score)
+    def _approx_bound(self, gamma):
+        """
+        Estimates the variational bound over *all documents* using only
+        the documents passed in as "docs." gamma is the set of parameters
+        to the variational distribution q(theta) corresponding to the
+        set of documents passed in.
+
+        The output of this function is going to be noisy, but can be
+        useful for assessing convergence.
+        """
+
+        # This is to handle the case where someone just hands us a single
+        # document, not in a list.
+        score = 0
+        Elogtheta = self._dirichlet_expectation(gamma)
+        expElogtheta = numpy.exp(Elogtheta)
+
+        # E[log p(docs | theta, beta)]
+        for doc_idx, doc in enumerate(self._docs):
+            # These are mostly just shorthand (but might help cache locality)
+            ids = doc.wordids
+            cts = doc.wordcts
+            gammad = gamma[doc_idx, :]
+            phinorm = numpy.zeros(len(ids))
+            for i in range(0, len(ids)):
+                temp = Elogtheta[doc_idx, :] + self._Elogbeta[:, ids[i]]
+                tmax = max(temp)
+                phinorm[i] = numpy.log(sum(numpy.exp(temp - tmax))) + tmax
+            score += numpy.sum(cts * phinorm)
+
+        # E[log p(theta | alpha) - log q(theta | gamma)]
+        score += numpy.sum((self._alpha - gamma) * Elogtheta)
+        score += numpy.sum(scipy.special.gammaln(gamma) - scipy.special.gammaln(self._alpha))
+        score += sum(scipy.special.gammaln(self._alpha * self._K) - scipy.special.gammaln(numpy.sum(gamma, 1)))
+
+        # E[log p(beta | eta) - log q (beta | lambda)]
+        score += numpy.sum((self._eta - self._lambda) * self._Elogbeta)
+        score += numpy.sum(scipy.special.gammaln(self._lambda) - scipy.special.gammaln(self._eta))
+        score += numpy.sum(scipy.special.gammaln(self._eta * self._W) - scipy.special.gammaln(numpy.sum(self._lambda, 1)))
+
+        return score
+
---
+++ Run_Example/Run_LDAVI.py
@@ -0,0 +1,17 @@
+from Models.LDA import VariationalEM
+import Utils.Documents
+
+
+if __name__ == '__main__':
+    """
+    Run LDA by CollapsedGibbs sampling to ap news
+    """
+    document_file_path = '../ap_news/ap.dat'
+    voca_file_path = '../ap_news/vocab.txt'
+
+    docs = Utils.Documents.Read_BOW_docs_for_VI(document_file_path)
+    vocas = Utils.Documents.Read_Voca_File(voca_file_path)
+
+    LDAModel = VariationalEM.VariationalEM(30, docs, vocas)
+    LDAModel.run(max_iter=10, do_print_log=True)
+    # LDAModel.ExportResultCSV('LDA_VI_ap_news_30')
